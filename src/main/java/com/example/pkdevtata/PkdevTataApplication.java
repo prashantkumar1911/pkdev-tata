4f0f2c35e50903d4959778fa9314ea1556726cf9
Prashant <prashant.kumar1911@gmail.com>
2026-06-12 04:45:30 +0530

    Initial Commit

---
+++ src/main/java/com/example/pkdevtata/PkdevTataApplication.java
@@ -0,0 +1,13 @@
+package com.example.pkdevtata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkdevTataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkdevTataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pkdevtata/PkdevTataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pkdevtata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkdevTataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
